688947e94f247975a244a3398a48b60ed980c58a
Gerald Versluis <gerald.versluis@microsoft.com>
2024-07-13 22:56:41 +0200

    Update GenericMessage.cs

--- src/Plugin.Maui.MessagingCenter/GenericMessage.cs
+++ src/Plugin.Maui.MessagingCenter/GenericMessage.cs
@@ -7,6 +7,6 @@ internal class GenericMessage(string message)
 
 internal class GenericMessage<T>(string message, T value)
 {
-    public string Message { get; } = message + "args";
+    public string Message { get; } = message;
     public T Value { get; } = value;
 }
